b03609b2c0a25c79cdeeb0b61e3a7cfaa9063012
spinkymax <sp1nk@mail.ru>
2022-05-24 22:27:53 +0300

    Memory storage

--- cache.go
+++ cache.go
@@ -3,21 +3,43 @@ package cache
 import "time"
 
 type Cache struct {
+	grades map[string]Account
+}
+type Account struct {
+	value    string
+	deadline time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	newgrades := Cache{grades: make(map[string]Account)}
+	return newgrades
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (from Cache) Put(key, value string) {
+	from.grades[key] = Account{value: value}
 
 }
+func (from Cache) Keys() []string {
+	var key []string
+	for keys, k := range from.grades {
+		if k.deadline.IsZero() || time.Now().Before(k.deadline) {
+			key = append(key, keys)
+		}
+	}
+	return key
 
-func (receiver) Put(key, value string) {
+}
+func (from Cache) Get(key string) (string, bool) {
+	if k, ok := from.grades[key]; ok {
+		if k.deadline.IsZero() || time.Now().Before(k.deadline) {
+			return k.value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Keys() []string {
-}
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+
+func (from Cache) PutTill(key, value string, deadline time.Time) {
+  from.grades[key] = Account{value: value,deadline: deadline }
 }
